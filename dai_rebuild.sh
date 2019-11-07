8e4d2ef3b2d04a19e4fc1ebd81141cdee1d989c7
Enrique Tasa <tasa@enriquetasa.com>
2019-11-07 11:49:32 +0000

    Changed dai_rebuild.sh to actually work.

--- dai_rebuild.sh
+++ dai_rebuild.sh
@@ -1,3 +1,3 @@
-echo "ps -ef | grep ".vscode-server" | xargs kill"
-echo "ls core.* | xargs rm"
-echo "sm_sysman noninteractive shutdown && ut_sys_build && sm_startup --yes-to-all"
+pgrep -f ".vscode-server" | xargs kill
+ls core.* | xargs rm
+sm_sysman noninteractive shutdown && ut_sys_build && sm_startup --yes-to-all
