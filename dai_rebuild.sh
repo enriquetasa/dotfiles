2f6f3f63ab8ea2ffbf225f051189744a5912c1bd
Enrique Tasa <tasa@enriquetasa.com>
2019-11-08 09:16:08 +0000

    Changed dai_rebuild.sh to filter vs-code processes more effectively.

--- dai_rebuild.sh
+++ dai_rebuild.sh
@@ -1,3 +1,3 @@
-pgrep -f ".vscode-server" | xargs kill
+pgrep -f ".vscode-server/bin" | xargs kill
 ls core.* | xargs rm
 sm_sysman noninteractive shutdown && ut_sys_build && sm_startup --yes-to-all
--- nts_setup.sh
+++ nts_setup.sh
@@ -1,4 +1,8 @@
 cd /
-git clone https://github.com/enriquetasa/nts.git
+
+if [ ! -d /nts ]; then
+  git clone https://github.com/enriquetasa/nts.git
+fi
+
 chmod -R 777 nts 
 cat /nts/nts.sh >> ~/.bashrc
