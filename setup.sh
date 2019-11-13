8510663db23a27b073a33fabe45e1b87c808d7da
Enrique Tasa <tasa@enriquetasa.com>
2019-11-13 10:42:16 +0000

    Changes to setup.sh to make it more efficient

--- setup.sh
+++ setup.sh
@@ -5,5 +5,7 @@ cp /tasa_utilities/cgdbrc ~/.cgdb/
 sh /tasa_utilities/nts_setup.sh
 cp /tasa_utilities/ignore ~/.ignore
 cp /tasa_utilities/bash_profile ~/.bash_profile
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -d ~/.vim/bundle/]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
