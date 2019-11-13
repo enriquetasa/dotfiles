97db49f6ad2531e22e6146532ebca551c81bc0fe
Enrique Tasa <tasa@enriquetasa.com>
2019-11-13 10:43:44 +0000

    Had forgotten a closing bracket.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ cp /tasa_utilities/cgdbrc ~/.cgdb/
 sh /tasa_utilities/nts_setup.sh
 cp /tasa_utilities/ignore ~/.ignore
 cp /tasa_utilities/bash_profile ~/.bash_profile
-if [ ! -d ~/.vim/bundle/]; then
+if [ ! -d ~/.vim/bundle/ ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
