c882b4a45bd1a145c1b9d2b06b88f0bf68ccb88d
Enrique Tasa <tasa@enriquetasa.com>
2019-12-12 12:52:55 +0000

    New way of doing things, mostly in file organisation
    setup.sh now copies a file into the
    home directory that points config files to the /tasa_utilities/
    folder, so that just one file holds all of configs.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
-cp /tasa_utilities/bashrc ~/.bashrc
-cp /tasa_utilities/screenrc ~/.screenrc
-cp /tasa_utilities/vimrc ~/.vimrc
-cp /tasa_utilities/cgdbrc ~/.cgdb/
-sh /tasa_utilities/nts_setup.sh
-cp /tasa_utilities/ignore ~/.ignore
-cp /tasa_utilities/bash_profile ~/.bash_profile
+cp /tasa_utilities/FilesToCopy/bashrc ~/.bashrc
+cp /tasa_utilities/FilesToCopy/screenrc ~/.screenrc
+cp /tasa_utilities/FilesToCopy/vimrc ~/.vimrc
+cp /tasa_utilities/FilesToCopy/cgdbrc ~/.cgdb/
+cp /tasa_utilities/FilesToCopy/ignore ~/.ignore
+cp /tasa_utilities/FilesToCopy/bash_profile ~/.bash_profile
 if [ ! -d ~/.vim/bundle/ ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
+sh /tasa_utilities/nts_setup.sh
