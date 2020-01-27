6cfa6bb931fd21528f40df4bd4cf4ebf4041e9dc
Enrique Tasa <tasa@enriquetasa.com>
2020-01-27 10:57:27 +0000

    Fixes

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo ":source /tasa_utilities/vimrc" > ~/.vimrc
 echo ". /tasa_utilities/screenrc" > ~/.screenrc
 echo ". /tasa_utilities/cgbdrc" > ~/.cgbdrc
 echo ". /tasa_utilities/ignore" > ~/.ignorerc
-echo ". /tasa_utilities/bashprofile" > ~/.bash_profile
+echo ". /tasa_utilities/bash_profile" > ~/.bash_profile
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
