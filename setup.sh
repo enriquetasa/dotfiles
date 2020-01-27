101252fd5ed55857cc240a77c4efba313e8262e8
Enrique Tasa <tasa@enriquetasa.com>
2020-01-27 10:34:36 +0000

    Changed the way setup works by just referring to the file in /tasa_utilities

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
-cp /tasa_utilities/FilesToCopy/bashrc ~/.bashrc
-cp /tasa_utilities/FilesToCopy/screenrc ~/.screenrc
-cp /tasa_utilities/FilesToCopy/vimrc ~/.vimrc
-cp /tasa_utilities/FilesToCopy/cgdbrc ~/.cgdb/
-cp /tasa_utilities/FilesToCopy/ignore ~/.ignore
-cp /tasa_utilities/FilesToCopy/bash_profile ~/.bash_profile
-if [ ! -d ~/.vim/bundle/ ]; then
+chmod -R 777 /tasa_utilities
+echo ". /tasa_utilities/bashrc" > ~/.bashrc
+echo ". /tasa_utilities/vimrc" > ~/.vimrc
+echo ". /tasa_utilities/screenrc" > ~/.screenrc
+echo ". /tasa_utilities/cgbdrc" > ~/.cgbdrc
+echo ". /tasa_utilities/ignore" > ~/.ignorerc
+echo ". /tasa_utilities/bashprofile" > ~/.bash_profile
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
-#sh /tasa_utilities/nts_setup.sh
+vim +PluginInstall +qall
