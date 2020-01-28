f9c64e9756f48186fd247511ec1550ae448367aa
Enrique Tasa <tasa@enriquetasa.com>
2020-01-28 13:10:43 +0000

    Changes to the setup file to include the tmux file

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ echo ". /tasa_utilities/screenrc" > ~/.screenrc
 echo ". /tasa_utilities/cgbdrc" > ~/.cgbdrc
 echo ". /tasa_utilities/ignore" > ~/.ignorerc
 echo ". /tasa_utilities/bash_profile" > ~/.bash_profile
+echo ". /tasa_utilities/tmux.conf" > ~/.tmux.conf
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
