b9eb0ae6c9805df19c41b755a0582b2a50f426f8
Enrique Tasa <tasa@enriquetasa.com>
2020-07-29 17:00:02 +0100

    Cleans up repository by deleting legacy files

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 echo "Remember to modify this file to input the correct username"
 echo ". /dotfiles/bashrc" > ~/.bashrc
 echo ":source /dotfiles/vimrc" > ~/.vimrc
-echo ":source /dotfiles/init.vim" > /home/joji/.config/nvim/init.vim
-echo ". /dotfiles/screenrc" > ~/.screenrc
-echo ". /dotfiles/cgbdrc" > ~/.cgbdrc
+# echo ":source /dotfiles/init.vim" > /home/joji/.config/nvim/init.vim
 echo ". /dotfiles/ignore" > ~/.ignorerc
 echo ". /dotfiles/bash_profile" > ~/.bash_profile
 echo "source-file /dotfiles/tmux.conf" > ~/.tmux.conf
