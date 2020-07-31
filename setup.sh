ea1e8468113741da59807c848bfa23a104baef0b
Enrique Tasa <tasa@enriquetasa.com>
2020-07-31 10:29:29 +0200

    New vimrc

--- setup.sh
+++ setup.sh
@@ -1,12 +1,5 @@
-echo ". /tasa_utilities/bashrc" > ~/.bashrc
-echo ":source /tasa_utilities/vimrc" > ~/.vimrc
-echo ". /tasa_utilities/screenrc" > ~/.screenrc
-echo ". /tasa_utilities/cgbdrc" > ~/.cgbdrc
-echo ". /tasa_utilities/ignore" > ~/.ignorerc
-echo ". /tasa_utilities/bash_profile" > ~/.bash_profile
-echo "source-file /tasa_utilities/tmux.conf" > ~/.tmux.conf
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
-vim +PluginInstall +qall
-echo "Remember to chmod the tasa_utilities directory and compile YCM stuff"
+echo ". /dotfiles/bashrc" > ~/.bashrc
+echo ":source /dotfiles/vimrc" > ~/.vimrc
+echo ". /dotfiles/bash_profile" > ~/.bash_profile
+echo "source-file /dotfiles/tmux.conf" > ~/.tmux.conf
+echo "Remember to chmod the dotfiles directory and compile YCM stuff"
