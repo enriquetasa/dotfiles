3de04047e34761266fdf50055ec147cdd707a8d0
Enrique Tasa <tasa@enriquetasa.com>
2020-09-30 11:10:59 +0100

    An update in preparation for new year at ihateironing
    Left some TODOs to deal with TMUX mostly

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 echo "Remember to modify this file to input the correct username"
 echo ". /dotfiles/bashrc" > ~/.bashrc
-echo ":source /dotfiles/vimrc" > ~/.vimrc
+# echo ":source /dotfiles/vimrc" > ~/.vimrc
 # echo ":source /dotfiles/init.vim" > /home/joji/.config/nvim/init.vim
 echo ". /dotfiles/ignore" > ~/.ignorerc
 echo ". /dotfiles/bash_profile" > ~/.bash_profile
 echo "source-file /dotfiles/tmux.conf" > ~/.tmux.conf
-echo "Check for the dotfiles directory permissions and the compilation of YCM"
