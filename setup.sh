19a515420558e608e59b825c8e2ff3f76f4a9141
Tasa <tasa@enriquetasa.com>
2020-10-03 14:33:35 +0100

    ZSH support added, TMUX, zshrc and vim new configurations

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
-echo ". /dotfiles/bashrc" > ~/.bashrc
-echo ":source /dotfiles/vimrc" > ~/.vimrc
-echo ". /dotfiles/bash_profile" > ~/.bash_profile
-echo "source-file /dotfiles/tmux.conf" > ~/.tmux.conf
-echo "Remember to chmod the dotfiles directory and compile YCM stuff"
+echo "Modify this file to allow for username and for selection of vim/neovim and run again"
+echo ". /Users/etasa/dotfiles/bashrc" > ~/.bashrc
+echo ". /Users/etasa/dotfiles/zshrc" > ~/.zshrc
+echo ":source /Users/etasa/dotfiles/vimrc" > ~/.vimrc
+mkdir /Users/etasa/.config/nvim/
+echo ":source /Users/etasa/dotfiles/init.vim" > /Users/etasa/.config/nvim/init.vim
+echo ". /Users/etasa/dotfiles/ignore" > ~/.ignorerc
+echo ". /Users/etasa/dotfiles/bash_profile" > ~/.bash_profile
+echo "source-file /Users/etasa/dotfiles/tmux.conf" > ~/.tmux.conf
